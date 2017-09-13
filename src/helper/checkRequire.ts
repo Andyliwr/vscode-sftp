f7b3abe250d2ca5a6f33626ad5540ca7bde582d4
DiKang Li <121960425@qq.com>
2017-09-13 13:35:17 +0800

    开始初步改造

--- src/helper/checkRequire.ts
+++ src/helper/checkRequire.ts
@@ -3,7 +3,7 @@ import * as vscode from 'vscode';
 export default function checkRequire(cmd) {
   return (...args) => {
     if (!vscode.workspace.rootPath) {
-      vscode.window.showErrorMessage('The SFTP extension requires to work with an opened folder.');
+      vscode.window.showErrorMessage('初始化andyliwr-ftp的时候你必须先打开一个项目.');
       return;
     }
 
